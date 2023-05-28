5c6be45d1ff0cb012c5a2bde00c6c27e9110987f
laercioferracini <laercio_ferracini@yahoo.com.br>
2023-05-28 12:25:36 -0300

    reformating

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from pydub.utils import make_chunks
 import sys
 
 # colocar o diretório que está o seu vídeo na variável path 
-path = "./exemplo.mp4" 
+path = "./exemplo.mp4"
 
 # convert mp4 paramp3
 
@@ -15,28 +15,27 @@ clip = mp.VideoFileClip(path).subclip()
 # essa função irá gerar um arquivo de áudio no diretório que você está rodando o script 
 clip.audio.write_audiofile("./audio.mp3")
 
-
 audio = AudioSegment.from_file("audio.mp3", "mp3")
 
-size = 180000 # o milisegundo para cortar o audio no maximo 3 min
+size = 180000  # o milisegundo para cortar o audio no maximo 3 min
 
-chunks = make_chunks(audio, size) # Corte o arquivo em pedaços
+chunks = make_chunks(audio, size)  # Corte o arquivo em pedaços
 
 for i, chunk in enumerate(chunks):
-   # Enumeration, i is the index, chunk is the cut file
-   # convert mp3 file to wav
-   chunk_name = "audio{0}.wav".format(i)
-   # save document
-   chunk.export(chunk_name, format="wav")
-   file_audio = sr.AudioFile("./" + chunk_name)
-   
-   # use the audio file as the audio source
-   r = sr.Recognizer()
-   with file_audio as source:
-      audio_text = r.record(source)
-      text = r.recognize_google(audio_text,language='pt-BR')
+    # Enumeration, i is the index, chunk is the cut file
+    # convert mp3 file to wav
+    chunk_name = "audio{0}.wav".format(i)
+    # save document
+    chunk.export(chunk_name, format="wav")
+    file_audio = sr.AudioFile("./" + chunk_name)
 
-   arq = open(chunk_name.replace('.wav', '') + '.txt','w')
-   arq.write(text)
-   arq.close()
-   print(text)
+    # use the audio file as the audio source
+    r = sr.Recognizer()
+    with file_audio as source:
+        audio_text = r.record(source)
+        text = r.recognize_google(audio_text, language='pt-BR')
+
+    arq = open(chunk_name.replace('.wav', '') + '.txt', 'w')
+    arq.write(text)
+    arq.close()
+    print(text)
